e7bcdf6a358607b8cc16cfe242f839a64eab0e01
2001kevin <silahisabungan7@gmail.com>
2022-04-19 05:18:34 +0800

    Update sign up dan sign in dengan firebase

--- app/src/main/java/com/projectprogmob/ticapp/SignInActivity.java
+++ app/src/main/java/com/projectprogmob/ticapp/SignInActivity.java
@@ -1,14 +1,18 @@
 package com.projectprogmob.ticapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
@@ -16,58 +20,59 @@ import com.projectprogmob.ticapp.R;
 import com.projectprogmob.ticapp.home.DashboardFragment;
 import com.projectprogmob.ticapp.home.HomeActivity;
 
-public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
-
-    EditText etUsername;
-    EditText etPassword;
-
-    DatabaseReference mDatabase;
-
+public class SignInActivity extends AppCompatActivity {
+    DatabaseReference mDatabase = FirebaseDatabase.getInstance("https://ticappproject-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in_activity);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference("User");
-
-        etUsername = (EditText) findViewById(R.id.et_username);
-        etPassword = (EditText) findViewById(R.id.et_password);
+        final EditText etUsername = findViewById(R.id.et_username);
+        final EditText etPassword = findViewById(R.id.et_password);
 
         Button btnSignin = findViewById(R.id.btn_signin);
-        btnSignin.setOnClickListener(this);
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String UsernameTxt = etUsername.getText().toString();
+                final String PasswordTxt = etPassword.getText().toString();
+
+                if(UsernameTxt.isEmpty() || PasswordTxt.isEmpty()){
+                    Toast.makeText(SignInActivity.this, "Please enter your username or password", Toast.LENGTH_SHORT).show();
+                }else{
+                    mDatabase.child("User").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.hasChild(UsernameTxt)){
+                                final String getPassword = dataSnapshot.child(UsernameTxt).child("password").getValue(String.class);
+                                if(getPassword.equals(PasswordTxt)){
+                                    Toast.makeText(SignInActivity.this, "Successfully Logged in", Toast.LENGTH_SHORT).show();
+                                    Intent toHome = new Intent(SignInActivity.this, HomeActivity.class);
+                                    startActivity(toHome);
+                                }else{
+                                    Toast.makeText(SignInActivity.this,"Your Password is incorrect", Toast.LENGTH_SHORT).show();
+                                }
+                            }else{
+                                Toast.makeText(SignInActivity.this,"Your Username do not match any account",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            }
+        });
 
         Button btnSignup = findViewById(R.id.btn_signup);
-        btnSignup.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.btn_signin){
-            String usernameValue = etUsername.getText().toString();
-            String pwValue = etPassword.getText().toString();
-
-            if(usernameValue.equals("")){
-                etUsername.setError("Tolong masukan username anda dengan benar!");
-                etUsername.requestFocus();
-            }else if(pwValue.equals("")){
-                etPassword.setError("Tolong masukan password anda dengan benar!");
-                etPassword.requestFocus();
-            }else{
-                pushLogin(usernameValue,pwValue );
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toSignUp = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(toSignUp);
             }
-
-        }else if(view.getId() == R.id.btn_signup){
-            Intent moveToSignup = new Intent(SignInActivity.this, SignUpActivity.class);
-            startActivity(moveToSignup);
-        }
-    }
-
-    private void pushLogin(String usernameValue, String pwValue) {
-        mDatabase.child(usernameValue).addValueEventListener( ValueEventListener{
-            protected void onDataChange Object dataSnapshot;
-            (dataSnapshot: DataSnapshot){
-               String user = dataSnapshot.getClass(User.class);
-            }
-        })
+        });
     }
 }
--- app/src/main/java/com/projectprogmob/ticapp/SignUpActivity.java
+++ app/src/main/java/com/projectprogmob/ticapp/SignUpActivity.java
@@ -1,29 +1,93 @@
 package com.projectprogmob.ticapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
-public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignUpActivity extends AppCompatActivity  {
+
+    //membuat objek database reference class ke firebase realtime database
+    DatabaseReference mDatabase = FirebaseDatabase.getInstance("https://ticappproject-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_activity);
 
+        final EditText etfullname = findViewById(R.id.etFullname);
+        final EditText etusername = findViewById(R.id.etUsername);
+        final EditText etemail = findViewById(R.id.etEmail);
+        final EditText etphone = findViewById(R.id.etPhone);
+        final EditText etpassword = findViewById(R.id.etPassword);
+        final EditText etconpassword = findViewById(R.id.etConPassword);
+
         ImageView back = findViewById(R.id.iv_back);
-        back.setOnClickListener(this);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toSignin = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(toSignin);
+            }
+        });
+
+        Button btnDaftar = findViewById(R.id.btn_daftar);
+        btnDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //MENGAMBIL DATA DARI EDIT TEXT KE STRING VARIABLE
+                final String fullnametxt = etfullname.getText().toString();
+                final String usernametxt = etusername.getText().toString();
+                final String emailtxt = etemail.getText().toString();
+                final String phonetxt = etphone.getText().toString();
+                final String passwordtxt = etpassword.getText().toString();
+                final String conpasswordtxt = etconpassword.getText().toString();
+
+                //CEK APAKAH USER MENGISI SEMUA FIELD NYA
+                if(fullnametxt.isEmpty() || usernametxt.isEmpty() || emailtxt.isEmpty() || phonetxt.isEmpty() || passwordtxt.isEmpty() || conpasswordtxt.isEmpty()){
+                    Toast.makeText(SignUpActivity.this, "Mohon isi semua field!", Toast.LENGTH_SHORT).show();
+                }else if(!passwordtxt.equals(conpasswordtxt)){
+                    Toast.makeText(SignUpActivity.this, "Password tidak match", Toast.LENGTH_SHORT).show();
+                }else {
+                    mDatabase.child("User").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            //cek apakah username sudah dipakai atau tidak
+                            if(dataSnapshot.hasChild(usernametxt)){
+                                Toast.makeText(SignUpActivity.this, "Username is already registered", Toast.LENGTH_SHORT).show();
+                            }else{
+                                mDatabase.child("User").child(usernametxt).child("username").setValue(fullnametxt);
+                                mDatabase.child("User").child(usernametxt).child("email").setValue(emailtxt);
+                                mDatabase.child("User").child(usernametxt).child("phone").setValue(phonetxt);
+                                mDatabase.child("User").child(usernametxt).child("password").setValue(passwordtxt);
+
+                                Toast.makeText(SignUpActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+                }
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view) {
-        if(view.getId()==R.id.iv_back){
-            Intent toSignin = new Intent(SignUpActivity.this, SignInActivity.class);
-            startActivity(toSignin);
-        }
-    }
 }
